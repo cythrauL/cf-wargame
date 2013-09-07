da3f35c8901ee33f2ca962298f2c1027b5e88464
nevermore <nevermore@joshuas-MacBook-Pro.local>
2013-09-07 17:35:18 +0100

    Done some more work on ropasaurus

--- src/levels/ropasaurusrandom/ropasaurusrandom.c
+++ src/levels/ropasaurusrandom/ropasaurusrandom.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 
 gadget_1:
   asm{
-    
+    "pushl %eip"
+    "popl %esp"
+    "ret"
   }
 
 gadget_2:
   asm{
-
+  
   }
 
+int main(int argc, char *argv[])
+{
+  char address[64];
+  printf("[+]Executing binary %s, main loaded at %p\n", argv[0], main);
+  usleep(500);
 
+  int fd = open(argv[1], O_RDONLY | O_NOFOLLOW);
+
+  read(fd, address, 128);
+
+  return (int)address;
+}
